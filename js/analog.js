47bf0b827952b0503b23865f38ed81462f9fd218
Behrouz <behrouzshakeri05@gmail.com>
2025-12-31 22:37:05 +0330

    fix: correct hour hand rotation calculation in analog clock

--- js/analog.js
+++ js/analog.js
@@ -4,7 +4,7 @@ function updateTimeAnalog() {
   let min = now.getMinutes();
   let sec = now.getSeconds();
   console.log(sec);
-  document.querySelector(".hours").style.transform = `rotateZ(${(min * 360) / 60}deg)`;
+  document.querySelector(".hours").style.transform = `rotateZ(${(hours % 12) * 30 + min * 0.5}deg)`;
   document.querySelector(".min").style.transform = `rotateZ(${(min * 360) / 60}deg)`;
   document.querySelector(".sec").style.transform = `rotateZ(${(sec * 360) / 60}deg)`;
   setTimeout(updateTimeAnalog, 1000);
